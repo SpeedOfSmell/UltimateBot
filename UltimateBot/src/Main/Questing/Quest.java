23705c782f88ddd1c9aaa3ac4406906cf3622629
SpeedOfSmell <vldmirea@gmail.com>
2017-01-28 21:16:54 -0500

    Small changes to match MethodProvider method changes

--- UltimateBot/src/Main/Questing/Quest.java
+++ UltimateBot/src/Main/Questing/Quest.java
@@ -1,7 +1,9 @@
 package Main.Questing;
 
 public abstract class Quest {
-
+	
+	public abstract String[][] getItemsToBuy();
+	
 	public abstract void execute() throws InterruptedException;
 	
 }
--- UltimateBot/src/Main/Questing/Questing.java
+++ UltimateBot/src/Main/Questing/Questing.java
@@ -3,6 +3,9 @@ package Main.Questing;
 import org.osbot.rs07.script.Script;
 
 import Main.Main;
+import Main.MethodProvider;
+import Main.Questing.Quests.ImpCatcher;
+import Main.Questing.Quests.SheepShearer;
 
 public class Questing extends Script{
 
@@ -11,7 +14,7 @@ public class Questing extends Script{
 	private String quest;
 	private boolean shouldBuyItems;
 	
-	private final String quests[] = {"IMP CATCHER", "WITCH'S POTION"};
+	private final String quests[] = {"SHEEP SHEARER", "IMP CATCHER", "WITCH'S POTION"};
 	
 	public Questing(Main s) {
 		this.s = s;
@@ -32,15 +35,25 @@ public class Questing extends Script{
 
 	@Override
 	public int onLoop() throws InterruptedException {
-		for (String q : quests) { //For each item q in quests
+		for (String q : quests) { //For each item q in the list of quests defined above
 			if (q.equals(quest.toUpperCase())) { //Check if it matches the current quest
 				s.log("Starting " + q + ".");
 				Quest questToDo = null;
 				switch (q) { //check the quest to do
 					case "IMP CATCHER":
-						questToDo = new ImpCatcher(s, shouldBuyItems);
+						questToDo = new ImpCatcher(s);
 						break;
+					case "SHEEP SHEARER":
+						questToDo = new SheepShearer();
 				}
+				
+				if (shouldBuyItems && !MethodProvider.useGrandExchange(questToDo.getItemsToBuy())) { // If we need to buy the items and if wasnt able to
+					s.log("Unable to buy items. Aborting.");
+					s.stop(false); //abort script				
+				} 
+				
+				Main.sleep(Main.random(500, 1000));
+				
 				if (questToDo != null)
 					questToDo.execute();
 				
