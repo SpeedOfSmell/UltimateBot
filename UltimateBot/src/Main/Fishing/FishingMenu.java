1f48b24c8719141eceba9d8e95c1093bc35b1c74
SpeedOfSmell <vldmirea@gmail.com>
2017-01-31 23:49:32 -0500

    Created barebones of fishing script

--- UltimateBot/src/Main/Fishing/FishingMenu.java
+++ UltimateBot/src/Main/Fishing/FishingMenu.java
@@ -3,13 +3,46 @@ package Main.Fishing;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+
+import org.osbot.rs07.api.map.Area;
+import org.osbot.rs07.api.map.constants.Banks;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class FishingMenu extends JDialog {
 
-	/**
-	 * Launch the application.
-	 */
+	public boolean exit;
+	public boolean shouldBank;
+	
+	public String areaChoice;
+	
+	public FishingArea fishingArea;
+	
+	class FishingArea {
+		public Area area;
+		public Area bankArea;
+		public String fishType;
+		public String secondaryFishType;
+		
+		public FishingArea(Area area, Area bankArea, String fishType, String secondaryFishType){
+			this.area = area;
+			this.bankArea = bankArea;
+			this.fishType = fishType;
+			this.secondaryFishType = secondaryFishType;
+		}
+	}
+	
+	private FishingArea[] fishingAreas = {
+			new FishingArea(new Area(3091, 3226, 3082, 3239), Banks.DRAYNOR, "Net", "Bait")
+	};
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -28,8 +61,52 @@ public class FishingMenu extends JDialog {
 	 * Create the dialog.
 	 */
 	public FishingMenu() {
-		setBounds(100, 100, 450, 300);
+		setModal(true);
+		setBounds(100, 100, 239, 147);
+		getContentPane().setLayout(null);
+		
+		JLabel lblArea = new JLabel("Area:");
+		lblArea.setBounds(10, 11, 46, 14);
+		getContentPane().add(lblArea);
+		
+		JComboBox<String> cmbArea = new JComboBox<String>();
+		cmbArea.setModel(new DefaultComboBoxModel<String>(new String[] {"Draynor Village - Net", "Barbarian Village - Lure", "Karamja - Cage", "Karamja - Harpoon"}));
+		cmbArea.setBounds(10, 33, 151, 20);
+		getContentPane().add(cmbArea);
+		
+		JLabel lblBank = new JLabel("Bank:");
+		lblBank.setBounds(189, 11, 46, 14);
+		getContentPane().add(lblBank);
+		
+		JCheckBox chkBank = new JCheckBox("");
+		chkBank.setSelected(true);
+		chkBank.setBounds(199, 33, 21, 20);
+		getContentPane().add(chkBank);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {				
+				fishingArea = fishingAreas[cmbArea.getSelectedIndex()];
+				areaChoice = cmbArea.getSelectedItem().toString();
+				
+				shouldBank = chkBank.isSelected();
+				
+				exit = false;
+				dispose();
+			}
+		});
+		btnStart.setBounds(10, 74, 89, 23);
+		getContentPane().add(btnStart);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				exit = true;
+				dispose();
+			}
+		});
+		btnExit.setBounds(124, 74, 89, 23);
+		getContentPane().add(btnExit);
 
 	}
-
 }
